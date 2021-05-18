d09fec1918aee98e7745194456364c3578157a99
jasondotjson <jason.siervo@gmail.com>
2021-05-18 08:01:23 +0800

    Modified the placing of React Spectrum Provider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import ToDoList from './components/ToDoList';
+import MainPage from './MainPage';
+import { Provider, defaultTheme } from '@adobe/react-spectrum';
 
 function App() {
   return (
-    <div className="App">
-      <ToDoList />
-    </div>
+    <Provider colorScheme="dark" theme={defaultTheme}>
+      <div className="App">
+        <ToDoList />
+      </div>
+    </Provider>
   );
 }
 
