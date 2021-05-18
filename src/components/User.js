d42077d2b12a760c188e4d1da6242d40cb972934
jasondotjson <jason.siervo@gmail.com>
2021-05-18 08:08:16 +0800

    Changed to onPress and added a theme provider

--- src/components/User.js
+++ src/components/User.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
+import {Provider, defaultTheme, Button} from '@adobe/react-spectrum';
 
 const Border = styled.div`
     border-style: solid;
     margin: 10px;
+    border-radius: 70px;
 `;
 
 function User({userInfo: { id, name, username, phone}, handleUserClick }) {
@@ -13,13 +15,15 @@ function User({userInfo: { id, name, username, phone}, handleUserClick }) {
     }
 
     return (
-        <Border>
-            <h2>[{id}] {name}, ({username})</h2>
-            {/* <p>{address}</p> */}
-            <p>{phone}</p>
+        <Provider colorScheme="dark" theme={defaultTheme}>
+            <Border>
+                <h2>[{id}] {name}, ({username})</h2>
+                {/* <p>{address}</p> */}
+                <p>{phone}</p>
 
-            <button style={{margin: '5px'}} onClick={handleClick}>Set as user</button>
-        </Border>
+                <Button variant="cta" style={{margin: '5px'}} onPress={handleClick}>Set as user</Button>
+            </Border>
+        </Provider>
     );
 }
 
